7675e5c0846588e761b08695d061577a2ea94982
Amarius Jones <amariusj@gmail.com>
2021-06-16 05:41:24 -0400

    First Commit

---
+++ src/components/FormCapture.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const FormCapture = () => {
+    return(
+        <div className="form-capture">
+            <h1>Amarius Jones Test Form Capture</h1>
+            <hr />
+            <form action="https://analytics.clickdimensions.com/forms/h/azwGgHQgrI0a4DFY7Tm6Mm" method="post">
+                <fieldset>
+                    <div className="field">
+                        <span>Email Address</span>
+                        <input type="text" name="email" id="email" />
+                    </div>
+                    <div className="field">
+                        <span>First Name</span>
+                        <input type="text" name="firstname" id="firstname" />
+                    </div>
+                    <div className="field">
+                        <span>Last Name</span>
+                        <input type="text" name="lastname" id="lastname" />
+                    </div>
+                    <button type="submit">Submit</button>
+                </fieldset>
+            </form>
+        </div>
+    )
+}
+
+export default FormCapture
