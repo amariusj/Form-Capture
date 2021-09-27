99f0ae065fa155ab0c64129b7a2ca3f29d209dc7
Amarius Jones <amariusj@gmail.com>
2021-09-27 12:27:51 -0400

    Remove Web Tracking

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <FormCapture />
-      <iframe src="http://web.jmpco.com/jmpcocom-aueaf/pages/4mqvxvnneeuu7waiscmufq.html?PageId=c61564e2cdf9eb1194ef002248231415" allowtransparency="true" width="100%" height="500px" type="text/html" frameborder="0" style={{border:0}} title="cd-form"></iframe>
     </div>
   );
 }
