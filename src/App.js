2555efc3fe1eb862a9aa4ec049c5b0c9c0bc6ad0
Amarius Jones <amariusj@gmail.com>
2022-04-06 23:00:23 -0400

    Added Customer's Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import FormCapture from './components/FormCapture'
 import CustomForm from './components/CustomForm'
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+ 
 function App() {
   return (
-    <div className="App">
-      <FormCapture />
-      <CustomForm />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={FormCapture} />
+          <Route path="/join" component={CustomForm} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
