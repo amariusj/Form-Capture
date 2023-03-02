864f4b62866be818623efe2ac111bbf883cf104b
Amarius Jones <amariusj@gmail.com>
2023-03-02 11:31:58 -0500

    Update CustomForm.js

--- src/components/CustomForm.js
+++ src/components/CustomForm.js
@@ -4,15 +4,15 @@ const CustomForm = () => {
     return(
         <div className="list">
             <div className="single-item">
-                <a href="https://www.calamos.com/globalassets/media/documents/market-insights/2023/01/outlkcom_january_2023.pdf" target="_blank">Calamos Investment Team Outlooks, January 2023</a>
+                <a href="https://www.calamos.com/globalassets/media/documents/market-insights/2023/01/outlkcom_january_2023.pdf" target="_blank" rel='noreferrer'>Calamos Investment Team Outlooks, January 2023</a>
                 <p>The Calamos investment teams are finding cause for cautious optimism as inflationary headwinds weaken. Macro themes and valuations will remain important in 2023, but we anticipate markets to expand their focus on company fundamentals, providing a tailwind for our research-driven approaches.</p>
             </div>
             <div className="single-item">
-                <a href="https://www.calamos.com/globalassets/media/documents/product-literature/sales-ideas/calamos-market-neutral-income-fund-compelling-diversification-benefits-within-a-fixed-income-allocation.pdf" target="_blank">CMNIX: Compelling Diversification Benefits within a Fixed Income Allocation</a>
+                <a href="https://www.calamos.com/globalassets/media/documents/product-literature/sales-ideas/calamos-market-neutral-income-fund-compelling-diversification-benefits-within-a-fixed-income-allocation.pdf" target="_blank" rel='noreferrer'>CMNIX: Compelling Diversification Benefits within a Fixed Income Allocation</a>
                 <p>We believe CMNIX can make a compelling contribution to an asset allocation, supported by its historically attractive correlative benefits and strong return profile.</p>
             </div>
             <div className="single-item">
-                <a href="https://www.calamos.com/globalassets/media/documents/product-literature/commentary/calamos-global-and-international-update-reasons-for-optimism-in-2023.pdf" target="_blank">Calamos Global and International Update: Reasons for Optimism in 2023</a>
+                <a href="https://www.calamos.com/globalassets/media/documents/product-literature/commentary/calamos-global-and-international-update-reasons-for-optimism-in-2023.pdf" target="_blank" rel='noreferrer'>Calamos Global and International Update: Reasons for Optimism in 2023</a>
                 <p>The Calamos Global Team explains why 2023 could begin a multiyear cycle of non-US market outperformance, led by the emerging markets.</p>
             </div>
         </div>
