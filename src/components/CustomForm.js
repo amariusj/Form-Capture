dfa0cfc0e49ee673988dbf6f8430a5b06d429f53
Amarius Jones <amariusj@gmail.com>
2023-05-12 04:32:15 -0400

    Update CustomForm.js

--- src/components/CustomForm.js
+++ src/components/CustomForm.js
@@ -2,19 +2,36 @@ import React from 'react'
 
 const CustomForm = () => {
     return(
-        <div className="list">
-            <div className="single-item">
-                <a href="https://www.calamos.com/globalassets/media/documents/market-insights/2023/01/outlkcom_january_2023.pdf" target="_blank" rel='noreferrer'>Calamos Investment Team Outlooks, January 2023</a>
-                <p>The Calamos investment teams are finding cause for cautious optimism as inflationary headwinds weaken. Macro themes and valuations will remain important in 2023, but we anticipate markets to expand their focus on company fundamentals, providing a tailwind for our research-driven approaches.</p>
-            </div>
-            <div className="single-item">
-                <a href="https://www.calamos.com/globalassets/media/documents/product-literature/sales-ideas/calamos-market-neutral-income-fund-compelling-diversification-benefits-within-a-fixed-income-allocation.pdf" target="_blank" rel='noreferrer'>CMNIX: Compelling Diversification Benefits within a Fixed Income Allocation</a>
-                <p>We believe CMNIX can make a compelling contribution to an asset allocation, supported by its historically attractive correlative benefits and strong return profile.</p>
-            </div>
-            <div className="single-item">
-                <a href="https://www.calamos.com/globalassets/media/documents/product-literature/commentary/calamos-global-and-international-update-reasons-for-optimism-in-2023.pdf" target="_blank" rel='noreferrer'>Calamos Global and International Update: Reasons for Optimism in 2023</a>
-                <p>The Calamos Global Team explains why 2023 could begin a multiyear cycle of non-US market outperformance, led by the emerging markets.</p>
-            </div>
+        <div className="form-capture">
+            <h1>Convergia Inc ticket #328892 Form Capture</h1>
+            <hr />
+            <form action="https://analytics-ca.clickdimensions.com/forms/h/apNahnbzUWU6sXhOwt0Ji9" method="post">
+                <fieldset>
+                    <div className="field">
+                        <span>Products</span>
+                        <input type="text" name="FC_Products" id="FC_Products" />
+                    </div>
+                    <div className="field">
+                        <span>Message</span>
+                        <textarea id="FC_Message" name="FC_Message" rows="4" cols="50" />
+                    </div>
+                    <div className="field">
+                        <span>First Name</span>
+                        <input type="text" name="FC_FirstName" id="FC_FirstName" />
+                    </div>
+                    <div className="field">
+                        <span>Last Name</span>
+                        <input type="text" name="FC_LastName" id="FC_LastName" />
+                    </div>
+                    <div className="field">
+                        <span>Email</span>
+                        <input type="email" name="FC_Email" id="FC_Email" />
+                    </div>
+                    
+                    <button type="submit">Submit</button>
+                </fieldset>
+            </form>
+            <hr />
         </div>
     )
 }
