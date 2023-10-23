efec1062da2d69668ee5e3bcebba37932340c78d
Amarius Jones <amariusj@gmail.com>
2023-10-23 13:37:23 -0400

    Update Custom Form Capture

--- src/components/CustomForm.js
+++ src/components/CustomForm.js
@@ -7,25 +7,21 @@ const CustomForm = () => {
         <div className="form-capture">
             <h1>Form Capture for Ticket #338271</h1>
             <hr />
-            <Form action="https://analytics-ca.clickdimensions.com/forms/h/aszGebAH2OEmSJ3BCEA07d" method="post">
-                <Form.Group className="mb-3" controlId="first_name">
+            <Form action="https://analytics-ca.clickdimensions.com/forms/h/a6MXkgx4Nv0iRrZKJwHgHK" method="post">
+                <Form.Group className="mb-3" controlId="firstnametest">
                     <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" placeholder="John" name="first_name" />
+                    <Form.Control type="text" placeholder="John" name="firstnametest" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="last_name">
+                <Form.Group className="mb-3" controlId="testlastname">
                     <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" placeholder="Doe" name="last_name" />
+                    <Form.Control type="text" placeholder="Doe" name="testlastname" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="Email Address">
+                <Form.Group className="mb-3" controlId="testemail">
                     <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="text" placeholder="johndoe@example.com" name="Email Address" />
+                    <Form.Control type="text" placeholder="johndoe@example.com" name="testemail" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="Country">
-                    <Form.Label>Country</Form.Label>
-                    <Form.Control type="text" placeholder="United States of America" name="Country" />
                 </Form.Group>
                 
                 <Button variant="primary" type="submit">Submit</Button>
