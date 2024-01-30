a03749c288aced1923e630528a1b621778aee7fc
Amarius Jones <amariusj@gmail.com>
2024-01-30 08:42:15 -0500

    Update CustomForm.js

--- src/components/CustomForm.js
+++ src/components/CustomForm.js
@@ -6,129 +6,41 @@ const CustomForm = () => {
     return(
         <div className="form-capture">
             
-            <h1>Ticket #340737: Website Contact Us</h1>
+            <h1>Ticket #342288: OBHG news email alert subscription (Sign Up For Our Blog)</h1>
             <hr />
-            <Form action="https://analytics.clickdimensions.com/forms/h/ame9CgwV0fEWPH5CGAtTgT" method="post">
+            <Form action="https://analytics.clickdimensions.com/forms/h/a5R8zjpyqhUGeGsur5kHAA" method="post">
 
-                <Form.Group className="mb-3" controlId="firstname">
+                <Form.Group className="mb-3" controlId="Firstname">
                     <Form.Label>First Name</Form.Label>
-                    <Form.Control required={true} type="text" placeholder="John" name="firstname" />
+                    <Form.Control required={true} type="text" placeholder="John" name="Firstname" />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="lastname">
+                <Form.Group className="mb-3" controlId="Lastname">
                     <Form.Label>Last Name</Form.Label>
-                    <Form.Control required={true} type="text" placeholder="Doe" name="lastname" />
+                    <Form.Control required={true} type="text" placeholder="Doe" name="Lastname" />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="emailaddress1">
+                <Form.Group className="mb-3" controlId="Emailaddress">
                     <Form.Label>Email Address</Form.Label>
-                    <Form.Control required={true} type="text" placeholder="johndoe@example.com" name="emailaddress1" />
+                    <Form.Control required={true} type="text" placeholder="johndoe@example.com" name="Emailaddress" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="companyname">
+                <Form.Group className="mb-3" controlId="Company_name">
                     <Form.Label>Company</Form.Label>
-                    <Form.Control required={true} type="text" placeholder="ClickDimensions" name="companyname" />
+                    <Form.Control required={true} type="text" placeholder="ClickDimensions" name="Company_name" />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="telephone1">
-                    <Form.Label>Phone Number</Form.Label>
-                    <Form.Control type="tel" placeholder="(123) 456-7890" name="telephone1" />
-                </Form.Group>
-
-                <Form.Label aria-label='hiit_inquirytype'>Select an inquiry</Form.Label>
-                <Form.Select required={true} aria-label="hiit_inquirytype" controlId="hiit_inquirytype" name="hiit_inquirytype" className='mb-3'>
-                    <option>Select an inquiry</option>
-                    <option value="Subcontractor Inquiry">Subcontractor Inquiry</option>
-                    <option value="Careers Inquiry">Careers Inquiry</option>
-                    <option value="Media Inquiry">Media Inquiry</option>
-                    <option value="General Inquiry">General Inquiry</option>
-                </Form.Select>
-
-                <Form.Group controlId='hiit_webformattachment' className="mb-3">
-                    <Form.Label>Form Attachment</Form.Label>
-                    <Form.Control type="file" name="hiit_webformattachment" />
-                </Form.Group>
-
-                <Form.Group controlId="notes_description" className="mb-3">
-                    <Form.Label>Notes</Form.Label>
-                    <Form.Control type="textarea" required={true} name="notes_description" placeholder='Add some notes...' />
+                <Form.Group controlId="Comments" className="mb-3">
+                    <Form.Label>Comments</Form.Label>
+                    <Form.Control type="textarea" required={true} name="Comments" placeholder='Add some notes...' />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">Submit</Button>
             </Form>
-
             <hr />
-            <h1>Ticket #340737: Test Form Capture</h1>
-            <hr />
-            <Form action="https://analytics.clickdimensions.com/forms/h/aRKo1E5ltNk6EMPzZgyA" method="post">
-
-                <Form.Group className="mb-3" controlId="firstname">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control required={true} type="text" placeholder="John" name="firstname" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="lastname">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control required={true} type="text" placeholder="Doe" name="lastname" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="emailaddress1">
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control required={true} type="text" placeholder="johndoe@example.com" name="emailaddress1" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="telephone1">
-                    <Form.Label>Phone Number</Form.Label>
-                    <Form.Control type="tel" placeholder="(123) 456-7890" name="telephone1" />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">Submit</Button>
-            </Form>
-
-            <h1>Ticket #340215: Test Form Capture</h1>
-            <hr />
-            <Form action="https://analytics.clickdimensions.com/forms/h/aBOI8RC3yaESB0gSHDKdDK" method="post">
-
-                <Form.Group className="mb-3" controlId="firstname">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control required={true} type="text" placeholder="John" name="firstname" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="lastname">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control required={true} type="text" placeholder="Doe" name="lastname" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="emailaddress1">
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control required={true} type="text" placeholder="johndoe@example.com" name="emailaddress1" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Select multiple className="mb-3" controlId="amarius-multiselect" name="amarius-multiselect">
-                    <option>Multi-Select</option>
-                    <option value="option 1">option 1</option>
-                    <option value="option 2">option 2</option>
-                    <option value="option 3">option 3</option>
-                </Form.Select>
-
-                <Form.Select className="mb-3" controlId="amarius-regular-select" name="amarius-regular-select">
-                    <option>Regular Select</option>
-                    <option value="option 1">option 1</option>
-                    <option value="option 2">option 2</option>
-                    <option value="option 3">option 3</option>
-                </Form.Select>
-
-                <Button variant="primary" type="submit">Submit</Button>
-            </Form>
 
         </div>
     )
